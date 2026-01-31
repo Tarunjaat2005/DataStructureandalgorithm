e21518f0acb85bad5c41d7f2e2c65f45317a8bbc
Tarun Singh <165502315+Tarunjaat2005@users.noreply.github.com>
2026-01-31 11:23:43 +0530

    Update 0234-palindrome-linked-list.java
    using stack

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.java
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.java
@@ -27,3 +27,36 @@ class Solution {
         return true;
     }
 }
+
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public boolean isPalindrome(ListNode head) {
+        Stack<Integer> stack = new Stack<>();
+        ListNode curr = head;
+        while(curr != null){
+            stack.push(curr.val);
+            curr = curr.next;
+        }
+        curr = head;
+        System.out.println(stack);
+        while(curr != null){
+            if(curr.val != stack.pop()){
+                return false;
+            }
+             System.out.println(curr.val);
+            curr = curr.next;
+        }
+        
+        return true;
+    }
+}
